62271ac6bd96a723a2f6b785beffae555a9f8ea7
kse55 <kse55@enitt.co.kr>
2024-09-02 16:55:18 +0900

    command.sh 수정

--- command.sh
+++ command.sh
@@ -3,5 +3,11 @@ cd /usr/local/bin/
 chmod 777 /usr/local/bin/iiot_gw
 chmod 777 /usr/local/bin/iiot_hub
 chmod 777 /usr/local/bin/command.sh
-iiot_hub &
-iiot_gw
+
+if [ ! -d /var/log/iiot ]; then
+        mkdir /var/log/iiot
+fi
+
+./iiot_gw -f -r iiot_gw.json &
+exit 0
+
