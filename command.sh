696f5ef5c61bd121032851c9a510a286370d74e4
kse55 <kse55@enitt.co.kr>
2024-09-03 16:31:44 +0900

    command.sh에서 백그라운드 실행 제거

--- command.sh
+++ command.sh
@@ -3,4 +3,4 @@ cd /usr/local/bin/
 chmod 777 /usr/local/bin/iiot_gw
 chmod 777 /usr/local/bin/iiot_hub
 chmod 777 /usr/local/bin/command.sh
-./iiot_gw -f -r iiot_gw.json &
+./iiot_gw -f -r iiot_gw.json
